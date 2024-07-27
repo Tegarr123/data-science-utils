71c7613a09f7b57059dd0aa4bb5935b3f85fe194
fadhino <fadhino987@gmail.com>
2024-07-27 19:02:37 +0700

    Encode Categorical

---
+++ preprocessing/helper.py
@@ -0,0 +1,10 @@
+def encode_categorical(dataframe:pd.DataFrame,
+                       feature_name:str|list):
+    if type(feature_name) is str:
+        get_encoded = pd.get_dummies(dataframe.loc[:,[feature_name]],dtype=int) # Get Encoded data
+        dataframe.drop(columns=[feature_name], axis=1, inplace=True)
+        dataframe[get_encoded.columns] = get_encoded
+    elif type(feature_name) is list:
+        get_encoded = pd.get_dummies(dataframe.loc[:,feature_name],dtype=int) # Get Encoded data
+        dataframe.drop(columns=feature_name, axis=1, inplace=True)
+        dataframe[get_encoded.columns] = get_encoded
