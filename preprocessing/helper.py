caaf3ea17e89c9a3f726738b58b111f39c24fa19
fadhino <fadhino987@gmail.com>
2024-07-28 18:41:54 +0700

    add df_to_dataloader helper function

--- preprocessing/helper.py
+++ preprocessing/helper.py
@@ -1,4 +1,8 @@
 import pandas as pd
+
+import pandas as pd
+import torch.utils.data as data_utils
+import torch
 def encode_categorical(dataframe:pd.DataFrame,
                        feature_name:str|list):
     if type(feature_name) is str:
@@ -9,3 +13,25 @@ def encode_categorical(dataframe:pd.DataFrame,
         get_encoded = pd.get_dummies(dataframe.loc[:,feature_name],dtype=int) # Get Encoded data
         dataframe.drop(columns=feature_name, axis=1, inplace=True)
         dataframe[get_encoded.columns] = get_encoded
+
+def df_to_dataloader(train_data:pd.DataFrame,
+                     test_data:pd.DataFrame,
+                     label_feature:str,
+                     batch_size:int=None
+                     ):
+    # dataframe to tensor
+    X_train = torch.tensor(train_data.drop(columns=label_feature,axis=1).values)
+    y_train = torch.tensor(train_data.loc[:,label_feature].values)
+    X_test = torch.tensor(test_data.drop(columns=label_feature, axis=1).values)
+    y_test = torch.tensor(test_data.loc[:, label_feature].values)
+    # Create torch dataset
+    train_tensor = data_utils.TensorDataset(X_train, y_train)
+    test_tensor = data_utils.TensorDataset(X_test, y_test)
+    # Create data loader
+    train_loader = data_utils.DataLoader(dataset=train_tensor,
+                                         batch_size=batch_size,
+                                         shuffle=True)
+    test_loader = data_utils.DataLoader(dataset=test_tensor,
+                                        batch_size=batch_size,
+                                        shuffle=True)
+    return train_loader,test_loader
